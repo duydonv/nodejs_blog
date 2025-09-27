6416424806c0cdfc57b10a6cc87890e297965e49
duydonv <phamduydong1000@gmail.com>
2025-09-27 17:24:31 +0700

    htpp_logger template_engine static_file scss

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const morgan = require('morgan')
+const path = require('path')
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', exphbs.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

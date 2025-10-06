9b068be7c854e7ae5d5fe59fd89216e058e27bec
duydonv <phamduydong1000@gmail.com>
2025-10-06 13:02:17 +0700

    query parameter POST method

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', exphbs.engine({
@@ -23,6 +26,16 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+
+  console.log(req.body)
+  res.send('')
 })
 
 app.listen(port, () => {
